fc583f5820488b604622a79016a8dfc7d7426708
somoschinicuil <63343816+somoschinicuil@users.noreply.github.com>
2022-10-17 21:37:23 +0200

    Diosito ayuda

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        Estoy arreglando esta mierda...porfa tengan paciencia
+      </header>
+    </div>
+  );
+}
+
+export default App;
